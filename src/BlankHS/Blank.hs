6aaf7eadb28873ba6d32e85e1cb0be7395f0b5f7
Eric Conlon <econlon@twitter.com>
2016-02-22 22:21:22 -0800

    test pass

--- src/BlankHS/Blank.hs
+++ src/BlankHS/Blank.hs
@@ -7,5 +7,13 @@ import qualified Control.Foldl as Fold
 import qualified Data.Text as T
 import Turtle
 
+{-
+Handy links:
+https://hackage.haskell.org/package/turtle
+https://hackage.haskell.org/package/foldl
+https://hackage.haskell.org/package/text
+https://hackage.haskell.org/package/containers
+-}
+
 one :: Int
 one = 1
--- test/Main.hs
+++ test/Main.hs
@@ -3,9 +3,18 @@
 module Main where
 
 import qualified BlankHS.Blank as Blank
+import Test.HUnit.Base
+import Test.HUnit.Text
+import Turtle
+
+{-
+Handy links:
+https://wiki.haskell.org/HUnit_1.0_User's_Guide
+https://hackage.haskell.org/package/HUnit
+-}
 
 someTest :: Test
-day1_1 = TestCase $ do
+someTest = TestCase $ do
   assertEqual "one is 1" Blank.one 1
 
 allTests :: Test
